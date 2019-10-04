429dbaaef2d515fcc825bc9f0a03c3ccd84c6c9f
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-04 22:34:21 +0530

    input variable:string(String)

--- src/question3/Question3.java
+++ src/question3/Question3.java
@@ -30,6 +30,7 @@ public class Question3 {
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
         scanner.close();
 
     }
