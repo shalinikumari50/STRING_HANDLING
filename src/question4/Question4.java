9b2b550104d3620b55f8c03e04ef06f7bc6368db
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-04 22:58:08 +0530

    override method:replaceVowels to replace vowels with '*', '^' and '!' occurrence-wise

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -7,4 +7,23 @@ public class Question4 extends Question3 {
         super(string);
     }
 
+    @Override
+    public void replaceVowels() {
+        char[] arr = {'a', 'e', 'i', 'o', 'u'};
+        int count = 0;
+        for (char var : arr) {
+            if (count == 0) {
+                setString(getString().replace(var, '*'));
+                count += 1;
+            } else if (count == 1) {
+                setString(getString().replace(var, '^'));
+                count += 1;
+            } else {
+                setString(getString().replace(var, '!'));
+                count = (count + 1) % 3;
+            }
+
+        }
+        System.out.println(getString());
+    }
 }
