2bb0c2832d649e339078c85c13e8f32b994f72bc
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-04 22:40:06 +0530

    add getter-setter methods and parameterized constructor Question4(String string)

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -2,4 +2,16 @@ package question4;
 
 public class Question4 {
     private String string;
+
+    public Question4(String string) {
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
 }
