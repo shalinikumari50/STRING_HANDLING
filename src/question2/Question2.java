807ce70cd5273cbb1bbfb5df50f2ddda18b31f3e
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-04 21:50:16 +0530

    add getter-setter methods and parameterized constructor Question2(String string, int index)

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -4,4 +4,24 @@ public class Question2 {
     private String string;
     private int index;
 
+    public Question2(String string, int index) {
+        this.string = string;
+        this.index = index;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
 }
