66561d8c85401ff28f9612281c9e1ceb87f6ecd6
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-05 07:04:06 +0530

    add parameterized constructor:Question7(String string)

--- src/question7/Question7.java
+++ src/question7/Question7.java
@@ -3,6 +3,10 @@ package question7;
 public class Question7 {
     private String string;
 
+    public Question7(String string) {
+        this.string = string;
+    }
+
     public String getString() {
         return string;
     }
