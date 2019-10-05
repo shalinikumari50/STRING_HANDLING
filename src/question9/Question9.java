434eedfc9aa8a781d731aeb6cb1d083801fd192d
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-05 19:00:32 +0530

    extend class:Question8 and generate parameterized constructor:Question9(String string)

--- src/question9/Question9.java
+++ src/question9/Question9.java
@@ -1,4 +1,9 @@
 package question9;
 
-public class Question9 {
+import question8.Question8;
+
+public class Question9 extends Question8 {
+    public Question9(String string) {
+        super(string);
+    }
 }
