86684832cb74feaaa9910588f814e01e03702b72
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-05 06:40:00 +0530

    add getter and setter methods

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -4,4 +4,19 @@ public class Question5 {
     private String string;
     private String substring;
 
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public String getSubstring() {
+        return substring;
+    }
+
+    public void setSubstring(String substring) {
+        this.substring = substring;
+    }
 }
