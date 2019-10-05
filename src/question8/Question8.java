a4e1e9816cd130c418ca4183210fc6d38cde4b31
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-05 18:31:40 +0530

    add getter & setter methods

--- src/question8/Question8.java
+++ src/question8/Question8.java
@@ -3,5 +3,11 @@ package question8;
 public class Question8 {
     private String string;
 
+    public String getString() {
+        return string;
+    }
 
+    public void setString(String string) {
+        this.string = string;
+    }
 }
