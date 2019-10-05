a9b9b3c003213e7cca8a1ccb3ebf7ec11a995790
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-05 19:41:06 +0530

    add getter & setter methods

--- src/question11/Question11.java
+++ src/question11/Question11.java
@@ -3,4 +3,20 @@ package question11;
 public class Question11 {
     private String string;
     private char ch;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public char getCh() {
+        return ch;
+    }
+
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
 }
